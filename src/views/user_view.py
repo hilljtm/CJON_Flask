08a09bdd550b2a5f67ef2af5994bb10d13335a3c
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-04-15 15:54:52 -0400

    Testing Print(ser_users)

--- src/views/user_view.py
+++ src/views/user_view.py
@@ -18,6 +18,7 @@ def custom_response(res, status_code):
 def get_all():
     users = UserModel.get_all_users()
     ser_users = user_schema.dump(users, many=True)
+    print(ser_users)
     return custom_response(ser_users, 200)
 
 # READ - Get one User
