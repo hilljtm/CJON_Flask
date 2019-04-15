b191ecbbbb64c2c42a7d3e33295c6232ba44f866
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-04-15 13:54:45 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import os
 from src.app import create_app
+from flask_heroku import Heroku
+
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+heroku = Heroku(app)
 
 if __name__ == '__main__':
-    env_name = os.getenv('FLASK_ENV')
-    app = create_app(env_name)
     app.run()
